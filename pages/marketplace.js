d52ea14b07c40e03a8fb07b193ca6c07f9f266e3
FLIGHTZONE SIGNALS <bgwero31@gmail.com>
2025-07-04 11:12:21 +0200

    Create marketplace.js

---
+++ pages/marketplace.js
@@ -0,0 +1,9 @@
+// pages/marketplace.js
+export default function Marketplace() {
+  return (
+    <main>
+      <h1>AFRIBASE MARKETPLACE</h1>
+      {/* Your full marketplace component goes here! */}
+    </main>
+  );
+}
