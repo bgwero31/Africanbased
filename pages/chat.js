7516671c20794fcf10a53e58ee84523b87e3922f
FLIGHTZONE SIGNALS <bgwero31@gmail.com>
2025-07-04 11:18:32 +0200

    Create chat.js

---
+++ pages/chat.js
@@ -0,0 +1,9 @@
+// pages/chat.js
+export default function Chat() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>AFRIBASE CHAT</h1>
+      <p>Private messaging & group chat coming soon!</p>
+    </div>
+  );
+}
